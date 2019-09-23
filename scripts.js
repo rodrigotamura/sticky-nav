59a0ad73e9b46c8febfaeed7ff6d9a6f09ae1fd4
Rodrigo Tamura <ti.tamura@restauramundo.com>
2019-09-23 08:58:41 -0300

    Sticky nav done

---
+++ scripts.js
@@ -0,0 +1,18 @@
+// grabbing nav
+const nav = document.querySelector("#main");
+
+// figuring out nav's top offset position
+const topOfNav = nav.offsetTop;
+
+function fixNav() {
+  // if scrollY exceed topOfNav, add fixed-nav style class
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + "px";
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.style.paddingTop = nav.offsetHeight + 0;
+    document.body.classList.remove("fixed-nav");
+  }
+}
+
+window.addEventListener("scroll", fixNav);
